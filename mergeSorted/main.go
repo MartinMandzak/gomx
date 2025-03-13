657940c71fbee69196377c57c90373da5523e6d6
Martin Mandzák <mandzak.martin3@gmail.com>
2025-03-13 10:42:56 +0100

    got bored

---
+++ mergeSorted/main.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+package main
+
+import "fmt"
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	res := &ListNode()
+}
+
+func main{
+
+}
